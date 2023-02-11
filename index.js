d410893f6beb760ef7750b1676383d2265d41950
MartinTI21 <124494475+MartinTI21@users.noreply.github.com>
2023-02-11 12:46:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let count = 0
+let ResultEl = document.getElementById("result-el")
+function add1(){
+    count += 1
+    ResultEl.textContent = count
+}
+function add2(){
+    count += 2
+    ResultEl.textContent = count
+}
+function add3(){
+    count += 3
+    ResultEl.textContent = count
+}
