2a838648283718a4b4c51ee470a2def8939bf517
MartinTI21 <124494475+MartinTI21@users.noreply.github.com>
2023-02-13 21:55:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ function add3(){
     count += 3
     ResultEl.textContent = count
 }
+let countg = 0
+let ResultElG = document.getElementById("guest-result-el")
+function add1g(){
+    countg += 1
+    ResultElG.textContent = countg
+}
+function add2g(){
+    countg += 2
+    ResultElG.textContent = countg
+}
+function add3g(){
+    countg += 3 
+    ResultElG.textContent = countg
+}
+function resetgame(){
+    count = 0
+    countg= 0
+    ResultEl.textContent = count
+    ResultElG.textContent = countg
+}
